c6f893bde9dd58beeb750be98a88ef1da9004895
Omo <okpebhoo@gmail.com>
2020-06-12 12:16:14 +0100

    main files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    users: []
+  }  
+
+  componentDidMount () {
+    const url = 'https://jsonplaceholder.typicode.com/users';
+
+    fetch(url)
+    .then(result => result.json())
+    .then(result => {
+      this.setState({
+        users: result,
+      })
+    });
+  }
+
+  render() {
+    const { users } = this.state;
+
+    console.log(users);
+    return (
+      
+      <div className="container mx- auto" >
+        {users.map(user => {
+          return (
+        <div className="card w-100 border-success text-center my-4" >
+            <div>
+              <div className="card-header">
+                {user.name}
+              </div>
+              <ul key={user.id} className="list-group list-group-flush">
+                <li className="list-group-item">Username: {user.username}</li>
+                <li className="list-group-item">Email: {user.email}</li>
+                <li className="list-group-item">Phone: {user.phone}</li>
+                <li className="list-group-item">Website: {user.website}</li>
+              </ul>
+              <div className="card-footer text-muted">
+                 Address: {user.address.street}, {user.address.suite}, {user.address.city}.
+              </div>
+            </div>
+        </div>
+          )
+        }
+        )}
+        
+      </div>
+    )
+  }
 }
 
 export default App;
